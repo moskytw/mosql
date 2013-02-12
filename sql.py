71be95d35ef2c6378febf914778c2e3003acc074
Mosky <mosky.tw@gmail.com>
2013-02-12 13:53:57 +0800

    renamed method and added templates

--- sql.py
+++ sql.py
@@ -4,20 +4,32 @@
 class SQL(dict):
 
     @classmethod
-    def insert_into(cls, table):
-        pass
+    def insert(cls, table):
+        sql = cls('insert into <table> <columns> values <values>')
+        return sql
 
     @classmethod
     def select(cls, *fields):
-        pass
+        sql = cls('select <columns> from <table> where <condictions> limit <limit> offset <offset>')
+        return sql
 
     @classmethod
     def update(cls, table):
-        pass
+        sql = cls('update <table> set <set> where <condictions>')
+        return sql
 
     @classmethod
-    def delete_from(cls, table):
-        pass
+    def delete(cls, table):
+        sql = cls('delete from <table> where <condictions>')
+        return sql
+
+    def __init__(self, template):
+
+        if isinstance(template, str):
+            self.template = tuple(template.split(' '))
+        else:
+            assert hasattr(template, '__iter__'), "argumnet 'template; must be str or iterable"
+            self.template = template
 
     def __setattr__(self, key, value):
         pass
