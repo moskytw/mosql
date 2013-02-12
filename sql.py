d0ec52beb843eb9a3c9fc57f9f0fcbcf74306dad
Mosky <mosky.tw@gmail.com>
2013-02-12 14:31:01 +0800

    made the template tuples clearer

--- sql.py
+++ sql.py
@@ -5,22 +5,42 @@ class SQL(dict):
 
     @classmethod
     def insert(cls, table):
-        sql = cls(('insert into', '<table>'), ('<columns>', ), ('values', '<values>'))
+        sql = cls(
+            ('insert into', '<table>'),
+            ('<columns>', ),
+            ('values', '<values>')
+        )
         return sql
 
     @classmethod
     def select(cls, *fields):
-        sql = cls(('select', '<columns>'), ('from', '<table>'), ('where', '<condictions>'), ('order by', '<order by>'), ('<asc>', ), ('<desc>', ), ('limit', '<limit>'), ('offset', '<offset>'))
+        sql = cls(
+            ('select', '<columns>'),
+            ('from', '<table>'),
+            ('where', '<condictions>'),
+            ('order by', '<order by>'),
+            ('<asc>', ),
+            ('<desc>', ),
+            ('limit', '<limit>'),
+            ('offset', '<offset>')
+        )
         return sql
 
     @classmethod
     def update(cls, table):
-        sql = cls(('update', '<table>'), ('set', '<set>'), ('where' '<condictions>'))
+        sql = cls(
+            ('update', '<table>'),
+            ('set', '<set>'),
+            ('where' '<condictions>')
+        )
         return sql
 
     @classmethod
     def delete(cls, table):
-        sql = cls(('delete from', '<table>'), ('where', '<condictions>'))
+        sql = cls(
+            ('delete from', '<table>'),
+            ('where', '<condictions>')
+        )
         return sql
 
     def __init__(self, *template_groups):
