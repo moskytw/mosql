9ced7dd1822c6a51f1139a2a6d1ae1371b5386cb
Mosky <mosky.tw@gmail.com>
2013-02-28 04:46:28 +0800

    completed the draft version

--- new-model.py
+++ new-model.py
@@ -2,8 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from itertools import groupby, izip
+from collections import defaultdict
 import psycopg2
+import sql
 
+class Column(list):
+
+    def __init__(self, model, name, x):
+        list.__init__(self, x)
+        self.model = model
+        self.name = name
+
+    def __setitem__(self, idx, x):
+        self.model.set(self.name, idx, x)
+
+    def __delitem__(self, idx):
+        raise TypeError("this operation is not supported'")
+
+    def append(self, x):
+        raise TypeError("this operation is not supported'")
+
+    def pop(self, x):
+        raise TypeError("this operation is not supported'")
 
 class ModelMeta(type):
 
@@ -18,8 +38,10 @@ class ModelMeta(type):
             Model.identity = Model.columns
 
         if Model.columns:
+
             Model.offsets = dict((k, i) for i, k in enumerate(Model.columns))
             Model.group_idxs = tuple(Model.offsets[k] for k in Model.group)
+            Model.group_set = set(Model.group)
             Model.identity_idxs = tuple(Model.offsets[k] for k in Model.identity)
 
         return Model
@@ -28,6 +50,7 @@ class Model(dict):
 
     __metaclass__ = ModelMeta
 
+    table    = None
     columns  = None
     group    = None
     identity = None
@@ -37,34 +60,79 @@ class Model(dict):
 
         keyfunc = lambda row: tuple(row[i] for i in cls.group_idxs)
         for grouped_vals, rows in groupby(rows, keyfunc):
+            yield cls(rows)
 
-            model = cls()
+    def __init__(self, rows):
 
-            for i, name in enumerate(cls.group):
-                model[name] = grouped_vals[i]
+        self.changes = defaultdict(dict)
 
-            for name, row in izip(cls.columns, izip(*rows)):
-                if name not in model:
-                    model[name] = row
+        rows = list(rows)
 
-            yield model
+        # TODO: It should be updated after saving
+        self.identity_vals = [tuple(row[i] for i in self.identity_idxs) for row in rows]
 
-    def __init__(self, x=None):
-        if x:
-            super(Model, self).__init__(x)
+        for name, col in izip(self.columns, izip(*rows)):
+            if name in self.group_set:
+                self[name] = col[0]
+            else:
+                self[name] = Column(self, name, col)
 
-    def items(self):
-        return ((name, mode[name]) for name in self.columns)
+    def add(self, **row_vals):
+
+        # auto fill group columns
+        for k in self.group:
+            if k not in row_vals:
+                row_vals[k] = self[k]
+
+        # update the columns
+        for k in self.columns:
+            # TODO: the default value must be DEFAULT in SQL
+            v = row_vals.get(k)
+            if k in self.group_set:
+                self[k] = v
+            else:
+                list.append(self[k], v)
+
+        self.changes.update({len(self.changes): row_vals})
+
+    def set(self, col_name, row_idx, val):
+        list.__setitem__(self[col_name], row_idx, val)
+        self.changes.update(((self.identity_vals[row_idx], {col_name: val}), ))
+
+    def remove(self, row_idx):
+
+        for k in self.columns:
+            if k in self.group_set:
+                pass
+            else:
+                list.pop(self[k], row_idx)
+
+        self.changes.update({self.identity_vals[row_idx]: None})
+
+    def save(self):
+        # TODO: It is just a rough draft
+        for identity_vals, vals in self.changes.items():
+            if isinstance(identity_vals, int):
+                print sql.insert(self.table, vals)
+            elif vals is None:
+                print sql.delete(self.table, dict(zip(self.identity, identity_vals)))
+            else:
+                print sql.update(self.table, dict(zip(self.identity, identity_vals)), vals)
+        # TODO: get the default value if there has any DEFAULT (may use returning?)
 
     def __repr__(self):
         return '%s(%s)' % (self.__class__.__name__, super(Model, self).__repr__())
 
 class User(Model):
+    table = 'user'
     columns = ('user_id', 'name')
+    identity = ('user_id', )
 
 class Detail(Model):
+    table = 'detail'
     columns = ('detail_id', 'user_id', 'key', 'val')
     group = ('user_id', 'key')
+    identity = ('detail_id', )
 
 if __name__ == '__main__':
 
@@ -86,10 +154,14 @@ if __name__ == '__main__':
     cur.execute("select * from details where user_id='mosky' and key='email' order by detail_id")
     detail = list(Detail.arrange(cur.fetchall()))[0]
     print detail
-    # TODO:
-    #detail['val'][0] = "rarely used"
-    #del detail['val'][0]
-    #detail.add_row('DEFALUT', 'me@mosky.tw')
-    #print detail
+    detail['val'][0] = "rarely used"
+    print detail
+    detail.remove(1)
+    print detail
+    detail.add(val='mosky.tw@gmail.com')
+    print detail
+    print
+
+    detail.save()
 
     conn.close()
