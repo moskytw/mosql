08c33e0e81ab7754134c560602cbc703e0725ccd
Mosky <mosky.tw@gmail.com>
2013-03-01 02:30:30 +0800

    replaced model.py with the newest

--- model.py
+++ model.py
@@ -1,304 +1,281 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict, MutableSequence, MutableMapping
-from itertools import groupby
+from itertools import izip, groupby
+from collections import MutableSequence, MutableMapping
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    OrderedDict = dict
+
+from abc import ABCMeta
+
 import sql
 
-class RowProxy(MutableMapping):
+class Row(MutableMapping):
 
     def __init__(self, model, row_idx):
         self.model = model
         self.row_idx = row_idx
 
-    # --- implement standard mutable sequence ---
-
     def __len__(self):
-        return self.model.col_len
+        return len(self.model.column_names)
 
     def __iter__(self):
-        for col_name in self.model.col_names:
+        for col_name in self.column_names:
             yield col_name
 
-    def __getitem__(self, col_idx_or_name):
-        return self.model.elem(self.row_idx, col_idx_or_name)
+    def __getitem__(self, col_name):
+        return self.model.__getelem__(self.row_idx, col_name)
 
-    def __setitem__(self, col_idx_or_name, val):
-        self.model.set_elem(self.row_idx, col_idx_or_name, val)
+    def __setitem__(self, col_name, val):
+        self.model.__setelem__(self.row_idx, col_name, val)
 
     def __delitem__(self, col_name):
-        raise TypeError('use model.remove() instead')
-
-    # --- end ---
+        raise TypeError('this operation is not supported')
 
     def __repr__(self):
-        return '<RowProxy for row %r: %r>' % (self.row_idx, dict(self))
+        return repr(dict(self))
 
-    def cond(self):
-        return dict((k, self[k]) for k in self.model.uni_col_names)
+class Column(MutableSequence):
 
-    def ident(self):
-        return tuple(self[k] for k in self.model.uni_col_names)
-
-class ColProxy(MutableSequence):
-
-    def __init__(self, model, col_idx_or_name):
+    def __init__(self, model, col_name):
         self.model = model
-        self.col_idx = self.model.to_col_idx(col_idx_or_name)
-
-    # --- implement standard mutable sequence ---
+        self.col_name = col_name
 
     def __len__(self):
         return self.model.row_len
 
     def __iter__(self):
-        for i in xrange(len(self)):
+        for i in xrange(self.model.row_len):
             yield self[i]
 
     def __getitem__(self, row_idx):
-        return self.model.elem(row_idx, self.col_idx)
+        return self.model.__getelem__(row_idx, self.col_name)
 
     def __setitem__(self, row_idx, val):
-        self.model.set_elem(row_idx, self.col_idx, val)
+        self.model.__setelem__(row_idx, self.col_name, val)
 
-    def __delitem__(self, row_idx):
-        raise TypeError('use model.remove() instead')
+    def __delitem__(self, col_name):
+        raise TypeError('this operation is not supported')
 
-    def insert(self, row_idx, val):
-        raise TypeError('use model.add() instead')
-
-    # --- end ---
+    def insert(self, col_name):
+        raise TypeError('this operation is not supported')
 
     def __repr__(self):
-        return '<ColProxy for col %r (%s): %r>' % (self.col_idx, self.model.col_names[self.col_idx], list(self))
+        return repr(list(self))
+
+class Change(object):
+
+    def __init__(self, row_identity_column_names, row_identity_values, row):
+        self.row_identity_column_names = row_identity_column_names
+        self.row_identity_values = row_identity_values
+        self.row = row
+
+    def get_condition(self):
+        try:
+            return dict(izip(self.row_identity_column_names, self.row_identity_values))
+        except TypeError:
+            return None
+
+class ModelMeta(ABCMeta):
+
+    def __new__(meta, name, bases, attrs):
+
+        Model = super(ModelMeta, meta).__new__(meta, name, bases, attrs)
+
+        if not Model.group_by:
+            Model.group_by = Model.column_names
+
+        Model.column_offsets_map = dict((k, i) for i, k in enumerate(Model.column_names))
+        group_by_idxs = tuple(Model.column_offsets_map[col_name] for col_name in Model.group_by)
+        Model.group_by_key_func = staticmethod(lambda row: tuple(row[i] for i in group_by_idxs))
+
+        return Model
 
 class Model(MutableMapping):
 
-    table = None
-    col_names = tuple()
-    uni_col_names = tuple()
-    grp_col_names = tuple()
-    ord_col_names = tuple()
+    __metaclass__ = ModelMeta
 
-    def __init__(self, grp_col=None):
-
-        if not hasattr(self, 'col_offsets'):
-            self.__class__.col_offsets = dict((col_name, i) for i, col_name in enumerate(self.col_names))
-
-        if grp_col:
-            self.grp_col_vals = dict(zip(self.grp_col_names, grp_col))
-        self._elems = []
-
-        self.col_len = len(self.col_names)
-        self.row_len = 0
-
-        self.added_rows = []
-        self.removed_row_conds = []
-        self.changed_row_conds = {}
-        self.changed_row_vals = defaultdict(dict)
-        self.changed_order = []
-
-    def load(self, rows):
-
-        for i, row in enumerate(rows):
-            self._elems.extend(row)
-
-        self.col_len = len(self.col_names)
-        self.row_len = len(self._elems) / self.col_len
+    table_name = ''
+    column_names = tuple()
+    identify_by = tuple()
+    order_by = tuple()
+    group_by = tuple()
 
     @classmethod
-    def find(cls, **where):
-        model = cls()
-        cur = cls.execute(sql=sql.select(cls.table, where, cls.col_names, order_by=cls.grp_col_names+cls.ord_col_names))
-        rows = cur.fetchall()
-        if not rows:
-            return None
-        elif not cls.grp_col_names or all(grp_col_name in where for grp_col_name in cls.grp_col_names):
-            model = cls()
-            model.load(rows)
-            return model
-        else:
-            grp_col_idxs = map(model.to_col_idx, cls.grp_col_names)
-            to_key = lambda row: map(row.__getitem__, grp_col_idxs)
-            models = []
-            for key, grouped_rows in groupby(rows, key=to_key):
-                print key
-                model = cls(key)
-                model.load(grouped_rows)
-                models.append(model)
-            return models
+    def group(cls, rows):
+        for grouped_row_vals, rows in groupby(rows, cls.group_by_key_func):
+            yield cls(dict(izip(cls.group_by, grouped_row_vals)), rows)
 
-    def to_col_idx(self, col_idx_or_name):
-        if isinstance(col_idx_or_name, basestring):
-            return self.col_offsets[col_idx_or_name]
-        else:
-            return col_idx_or_name
+    def __init__(self, grouped_row, rows):
 
-    def to_col_name(self, col_idx_or_name):
-        if isinstance(col_idx_or_name, (int, long)):
-            return self.col_names[col_idx_or_name]
-        else:
-            return col_idx_or_name
+        self.grouped_row = grouped_row
 
-    def to_elem_idx(self, row_idx, col_idx_or_name):
-        return row_idx * self.col_len + self.to_col_idx(col_idx_or_name)
+        self.row_len = 0
+        self.elems = []
+        for row in rows:
+            self.elems.extend(row)
+            self.row_len += 1
 
-    # --- implement standard mutable sequence ---
+        self.proxies = {}
+        self.changes = OrderedDict()
 
     def __len__(self):
-        return self.col_len
+        return len(self.column_names)
 
     def __iter__(self):
-        for col_name in self.col_names:
+        for col_name in self.column_names:
             yield col_name
 
     def __getitem__(self, x):
-        if isinstance(x, basestring):
-            if x in self.grp_col_names:
-                return self.grp_col_vals.get(x)
-            else:
-                return self.col(x)
-        elif isinstance(x, (int, long)):
-            return self.row(x)
 
-    def __setitem__(self, grp_col_name, val):
-        if grp_col_name not in self.grp_col_names: return
+        if x in self.grouped_row:
+            return self.grouped_row[x]
 
-        elem_idx = self.to_elem_idx(0, grp_col_name)
+        if isinstance(x, int) and x < 0:
+            x += self.row_len
 
-        if grp_col_name not in self.changed_row_conds:
-            self.changed_row_conds[grp_col_name] = {grp_col_name: self._elems[elem_idx]}
-            self.changed_order.append(grp_col_name)
-        self.changed_row_vals[grp_col_name][grp_col_name] = val
+        if x in self.proxies:
+            return self.proxies[x]
 
-        self.grp_col_vals[grp_col_name] = val
+        if isinstance(x, str):
+            Proxy = Column
+        else:
+            Proxy = Row
 
-    def __delitem__(self, x, val):
-        pass
+        self.proxies[x] = proxy = Proxy(self, x)
+        return proxy
 
-    # --- end ---
+    def __setitem__(self, grouped_col_name, val):
 
-    def row(self, i):
-        return RowProxy(self, i)
+        row_identity_values = tuple(self.grouped_row[col_name] for col_name in self.group_by)
+        if row_identity_values in self.changes:
+            self.changes[row_identity_values].row[grouped_col_name] = val
+        else:
+            self.changes[row_identity_values] = Change(self.group_by, row_identity_values, {grouped_col_name: val})
 
-    def col(self, col_idx_or_name):
-        return ColProxy(self, col_idx_or_name)
+        self.grouped_row[grouped_col_name] = val
 
-    def elem(self, row_idx, col_idx_or_name):
-        return self._elems[self.to_elem_idx(row_idx, col_idx_or_name)]
+    def __getelem__(self, row_idx, col_name):
+        return self.elems[row_idx*len(self.column_names)+self.column_offsets_map[col_name]]
+
+    def get_row_identity_values(self, row_idx):
+        return tuple(self[row_idx][col_name] for col_name in self.identify_by)
+
+    def __setelem__(self, row_idx, col_name, val):
+
+        row_identity_values = self.get_row_identity_values(row_idx)
+        if row_identity_values in self.changes:
+            self.changes[row_identity_values].row[col_name] = val
+        else:
+            self.changes[row_identity_values] = Change(self.identify_by, row_identity_values, {col_name: val})
+
+        self.elems[row_idx*len(self.column_names)+self.column_offsets_map[col_name]] = val
+
+    def __delitem__(self, col_name):
+        raise TypeError('this operation is not supported')
+
+    def append(self, **row):
+
+        for col_name in self.group_by:
+            if col_name not in row:
+                row[col_name] = self.grouped_row[col_name]
+
+        self.changes[len(self.changes)] = Change(None, None, row)
+
+        for col_name in self.column_names:
+            self.elems.append(row.get(col_name))
+        self.row_len += 1
+
+    def pop(self, row_idx=-1):
+        row_identity_values = self.get_row_identity_values(row_idx)
+        self.changes[row_identity_values] = Change(self.identify_by, row_identity_values, None)
+
+        self.row_len -= 1
+        col_len = len(self.column_names)
+        start = row_idx * col_len
+        del self.elems[start:start+col_len]
+
+    def clear(self):
+        for i in xrange(self.row_len):
+            self.pop()
 
     def rows(self):
         for i in xrange(self.row_len):
-            yield self.row(i)
+            return self[i]
 
-    def cols(self):
-        for col_name in self.col_names:
-            yield self.col(col_name)
-
-    def set_elem(self, row_idx, col_idx_or_name, val):
-
-        row = self[row_idx]
-        col_idx   = self.to_col_idx(col_idx_or_name)
-        col_name  = self.to_col_name(col_idx_or_name)
-        row_ident = row.ident()
-
-        if row_ident not in self.changed_row_conds:
-            self.changed_row_conds[row_ident] = row.cond()
-            self.changed_order.append(row_ident)
-
-        self.changed_row_vals[row_ident][col_name] = val
-        self._elems[self.to_elem_idx(row_idx, col_idx_or_name)] = val
-
-    def add(self, **vals):
-        vals.update(self.grp_col_vals)
-        row = [None] * self.col_len
-        for col_name, offset in self.col_offsets.items():
-            row[offset] = vals[col_name]
-        self.add_row(row)
-
-    def add_row(self, row):
-        self.added_rows.append(row)
-        self._elems.extend(row)
-        self.row_len += 1
-
-    def remove_row(self, row_idx):
-        self.removed_row_conds.append(self[row_idx].cond())
-        self.row_len -= 1
-        start = row_idx * self.col_len
-        del self._elems[start:start+self.col_len]
-
-    @staticmethod
-    def execute(self, sql=None, sqls=None):
-        if sql:
-            print sql
-        if sqls:
-            for sql in sqls:
-                print sql
-
-    def commit(self):
-        sqls = []
-        for row in self.added_rows:
-            sqls.append(sql.insert(self.table, self.col_names, row))
-        for row_cond in self.removed_row_conds:
-            sqls.append(self.execute(sql.delete(self.table, row_cond)))
-        for k in self.changed_order:
-            sqls.append(sql.update(self.table, self.changed_row_conds[k], self.changed_row_vals[k]))
-        self.execute(sqls=sqls)
+    def save(self):
+        for change in self.changes.values():
+            cond = change.get_condition()
+            if cond is None:
+                print sql.insert(self.table_name, change.row)
+            elif change.row is None:
+                print sql.delete(cond)
+            else:
+                print sql.update(self.table_name, cond, change.row)
 
     def __repr__(self):
-        return '<%s %s>' % (self.__class__.__name__, dict(self))
+        return repr(dict(self))
 
-from psycopg2 import pool
-pool = pool.SimpleConnectionPool(1, 5, database='mosky')
+class Person(Model):
+    table_name = 'person'
+    column_names = ('person_id', 'name')
+    identify_by = ('person_id', )
 
-class PostgreSQLModel(Model):
-
-    @staticmethod
-    def execute(sql=None, sqls=None):
-        conn = pool.getconn()
-        cur = conn.cursor()
-        if sql:
-            print sql
-            cur.execute(sql)
-        if sqls:
-            for sql in sqls:
-                print sql
-                cur.execute(sql)
-        conn.commit()
-        conn = pool.putconn(conn)
-        return cur
-
-class User(PostgreSQLModel):
-
-    table = 'users'
-    col_names = ('user_id', 'name')
-    uni_col_names = ('user_id', )
-
-class Detail(PostgreSQLModel):
-
-    table = 'details'
-    col_names = ('detail_id', 'user_id', 'key', 'val')
-    uni_col_names = ('detail_id', )
-    grp_col_names = ('user_id', 'key')
-    ord_col_names = ('detail_id', )
+class Detail(Model):
+    table_name = 'detail'
+    column_names = ('detail_id', 'person_id', 'key', 'val')
+    identify_by = ('detail_id', )
+    group_by = ('person_id', 'key')
 
 if __name__ == '__main__':
 
+    import psycopg2
     from pprint import pprint
 
-    details = Detail.find(user_id='mosky')
-    for detail in details:
-        print detail
+    conn = psycopg2.connect(database='mosky')
+    cur = conn.cursor()
 
-    detail = details[1]
+    # --- test 1:1 table ---
+
+    cur.execute('select * from person order by person_id')
+    for person in Person.group(cur.fetchall()):
+        pprint(person)
+    print
+
+    person['name'] = 'New Name'
+    print person
+    print
+
+    person.clear()
+    print person
+    print
+
+    person.save()
+    print
+
+    # --- test 1:n (n:n) table ---
+
+    cur.execute('select * from detail order by person_id, key')
+    for detail in Detail.group(cur.fetchall()):
+        pprint(detail)
+    print
+
+    detail['val'][0] = 'new@email.com'
     print detail
-    detail['key'] = 'emails'
-    detail['val'][0] = 'test@gmail.com'
-    detail.commit()
+    print
 
-    #detail = Detail(('mosky', 'email'))
-    #detail.add(detail_id=11, val='newmail@mosky.tw')
-    #detail.commit()
+    detail.pop(0)
+    print detail
+    print
 
-    user = User.find(user_id='mosky')
-    print user
+    detail.append(val='new@email.com')
+    print detail
+    print
+
+    detail.save()
+    print
+
+    conn.close()
