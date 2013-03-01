b8b7fdba6617982c20d4b8d1b103b853095e47a4
Mosky <mosky.tw@gmail.com>
2013-03-01 18:46:11 +0800

    added tests

---
+++ tests/unittest_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class TestModel(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(1, 1)
+
+    def test_dummy2(self):
+        self.assertEqual(1, 1)
+
+    def test_dummy3(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
