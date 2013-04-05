094c132b353290fff925253aaa28dd0fce3e2661
Mosky <mosky.tw@gmail.com>
2013-04-06 06:52:30 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from distutils.core import setup
 
-long_description = open('README.rst').read()
-
 from mosql import __version__
 
 setup(
     name    = 'mosql',
     description = 'MoSQL is a lightweight Python library which assists programmer to use SQL.',
-    long_description = long_description,
+    long_description = open('README.rst').read(),
     version = __version__,
     author  = 'Mosky',
     author_email = 'mosky.tw@gmail.com',
     url = 'http://mosql.mosky.tw/',
     packages = ['mosql'],
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database'
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
+
