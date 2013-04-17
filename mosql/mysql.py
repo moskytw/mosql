16acfdd886836b9e41d12badcad23aaa2f656715
Mosky <mosky.tw@gmail.com>
2013-04-17 19:00:10 +0800

    added the escape for mysql

--- mosql/mysql.py
+++ mosql/mysql.py
@@ -12,6 +12,52 @@ Usage:
 It replaces the function in :mod:`mosql.util` with it.
 '''
 
+char_escape_map = {
+    # escape chars in the official libaray
+    '\0' : r'\0',
+    '\n' : r'\n',
+    '\r' : r'\r',
+    '\\' : r'\\',
+    '\"' : r'\"',
+    '\x1A' : r'\Z',
+    # escape chars in the esapi of OWASP
+    '\b' : r'\b',
+    '\t' : r'\t',
+    '\'' : r'\'',
+    '%'  : r'\%',
+    '_'  : r'\_',
+}
+
+def escape(s):
+    '''The function is designed for MySQL.
+
+    >>> tmpl = "select * from person where person_id = '%s';"
+    >>> evil_value = "' or true; --"
+
+    >>> print tmpl % escape(evil_value)
+    select * from person where person_id = '\\'\ or\ true\;\ \-\-';
+    '''
+
+    global char_escape_map
+
+    escapes = []
+
+    for c in s:
+
+        if ord(c) >= 256:
+            # c is not a 8-bit char
+            escapes.append(c)
+            continue
+
+        if c.isalnum():
+            escapes.append(c)
+            continue
+
+        escape = char_escape_map.get(c)
+        escapes.append(escape if escape else c)
+
+    return ''.join(escapes)
+
 def delimit_identifier(s):
     '''Enclose the identifier, `s`, by ` (back-quote).'''
     return '`%s`' % s
@@ -21,6 +67,8 @@ def escape_identifier(s):
     return s.replace('`', '``')
 
 import mosql.util
+
+mosql.util.escape = escape
 mosql.util.delimit_identifier = delimit_identifier
 mosql.util.escape_identifier = escape_identifier
 
