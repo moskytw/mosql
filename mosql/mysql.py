2f3d93175c351f83ea0f619048e8b63fa85fd833
Mosky <mosky.tw@gmail.com>
2013-04-17 22:30:02 +0800

    grafted the new escape functions from rewrite-util

--- mosql/mysql.py
+++ mosql/mysql.py
@@ -1,55 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''It contains the utils for MySQL.
+'''It applies the MySQL-specific stuff to :mod:`mosql.util`.
 
-After import it, it replaces the :py:func:`mosql.util.escape` with its :py:func:`mosql.mysql.escape`.'''
+Usage:
 
-__all__ = ['escape']
+::
 
-ESCAPE_CHAR_MAP = {
-        0x00 : r'\0',
-        0x08 : r'\b',
-        0x09 : r'\t',
-        0x0a : r'\n',
-        0x0d : r'\r',
-        0x1a : r'\Z',
-        0x22 : r'\"',
-        0x25 : r'\%',
-        0x27 : r"\'",
-        0x5c : r'\\',
-        0x5f : r'\_',
-    }
+    import mosql.mysql
+
+It will replace the function in :mod:`mosql.util` with it.
+'''
+
+char_escape_map = {
+    # The following 7 chars is escaped in MySQL Connector/C (0.6.2)
+    '\0' : r'\0',
+    '\n' : r'\n',
+    '\r' : r'\r',
+    '\\' : r'\\',
+    '\'' : r'\'',
+    '\"' : r'\"',
+    '\x1A' : r'\Z',
+    # The following 4 chars is escaped in OWASP Enterprise Security API (1.0)
+    '\b' : r'\b',
+    '\t' : r'\t',
+    #'%'  : r'\%',
+    #'_'  : r'\_',
+    # The above 2 chars shouldn't be escaped, because '\%' and '\_' evaluate
+    # to string '\%' and '\_' outside of pattern-matching contexts. Programmers
+    # should take responsibility for escaping them in pattern-matching contexts.
+}
 
 def escape(s):
-    '''It is the escaping function designed for the MySQL mode.
+    '''The function is designed for MySQL.
 
-    >>> sql_tmpl = "SELECT * FROM member WHERE name = '%s' AND email = '...';"
-    >>> val = "'; DROP TABLE member; --"
-    >>> print sql_tmpl % escape(val)
-    SELECT * FROM member WHERE name = '\\'\;\ DROP\ TABLE\ member\;\ \-\-' AND email = '...';
+    >>> tmpl = "select * from person where person_id = '%s';"
+    >>> evil_value = "' or true; --"
+
+    >>> print tmpl % escape(evil_value)
+    select * from person where person_id = '\\' or true; --';
     '''
+    global char_escape_map
+    return ''.join(char_escape_map.get(c) or c for c in s)
 
-    escaped_chars = []
+def fast_escape(s):
+    '''This function only escapes the ' (single-quote) and \ (backslash).
 
-    for c in s:
+    It is enough for security and correctness, and it is faster 50x than using
+    the :func:`escape`.
 
-        if ord(c) >= 256:
-            escaped_chars.append(c)
-            continue
-
-        if c.isalnum():
-            escaped_chars.append(c)
-            continue
-
-        escaped_char = ESCAPE_CHAR_MAP.get(ord(c))
-        escaped_chars.append('\\'+c if escaped_char is None else escaped_char)
-
-    return ''.join(escaped_chars)
+    It is used for replacing the :func:`mosql.util.escape` if you import this
+    moudle.
+    '''
+    return s.replace('\\', '\\\\').replace("'", r"\'")
 
 import mosql.util
-mosql.util.escape = escape
+mosql.util.escape = fast_escape
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+    #from timeit import timeit
+    #from functools import partial
+
+    #timeit = partial(timeit, number=100000)
+    #bytes = ''.join(chr(i) for i in range(256))
+
+    #def _escape(s):
+    #    return s.replace("'", "''")
+
+    #print timeit(lambda: _escape(bytes))
+    ## -> 0.118767976761
+
+    #print timeit(lambda: escape(bytes))
+    ## -> 7.97847890854
+
+    #print timeit(lambda: fast_escape(bytes))
+    ## -> 0.155963897705
