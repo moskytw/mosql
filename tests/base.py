0c659bc8aa31338c65cc1f356312f0ef963ad715
Mosky <mosky.tw@gmail.com>
2013-05-23 23:33:30 +0800

    expaned the getconn and putconn

--- tests/base.py
+++ tests/base.py
@@ -5,5 +5,11 @@ import psycopg2
 from mosql.result2 import Model
 
 class PostgreSQL(Model):
-    getconn = classmethod(lambda cls: psycopg2.connect(database='mosky'))
-    putconn = classmethod(lambda cls, conn: None)
+
+    @staticmethod
+    def getconn():
+        return psycopg2.connect(database='mosky')
+
+    @staticmethod
+    def putconn(conn):
+        pass
