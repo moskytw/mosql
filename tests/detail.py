1bd01dc07dc7d20b37035aba879e3469e883b7ad
Mosky <mosky.tw@gmail.com>
2013-05-23 23:21:23 +0800

    refined the detail.py

--- tests/detail.py
+++ tests/detail.py
@@ -18,33 +18,43 @@ if __name__ == '__main__':
         print detail
     print
 
+
     print '# modified an email'
 
     mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
+
+    # you have to use this form to make model remeber the changes
     mosky_detail['val', 0] = 'this email is modified 1'
     mosky_detail['val', 0] = 'this email is modified 2'
     mosky_detail['val', 0] = 'this email is modified'
     mosky_detail.save()
 
+    # re-select to ensure the data modified is saved to db
     mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
-    print mosky_detail.val
-    print mosky_detail['val', 0]
+    print 'mails:     ', mosky_detail.val
+    print 'first mail:', mosky_detail.val[0]
+
     print
 
+
     print '# append'
+
     mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
     mosky_detail.append({'val': 'it is the new email'})
     mosky_detail.save()
 
     mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
-    print mosky_detail.val
+    print 'mails:', mosky_detail.val
+
     print
 
-    print '# pop'
+
+    print '# pop 0'
     mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
     mosky_detail.pop(0)
     mosky_detail.save()
 
     mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
-    print mosky_detail.val
+    print 'mails:', mosky_detail.val
+
     print
