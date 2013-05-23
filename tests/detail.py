a61ce3a8d1742eb41a1a64a0cc97f5ce2cf63508
Mosky <mosky.tw@gmail.com>
2013-05-23 21:49:31 +0800

    added test in detail.py

--- tests/detail.py
+++ tests/detail.py
@@ -10,4 +10,23 @@ class Detail(PostgreSQL):
     ident_by = ('detail_id', )
 
 if __name__ == '__main__':
-    pass
+
+    from pprint import pprint
+
+    print '# powerful arrange'
+    for detail in Detail.arrange():
+        print detail
+    print
+
+    print '# modified an email'
+
+    mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
+    mosky_detail['val', 0] = 'this email is modified 1'
+    mosky_detail['val', 0] = 'this email is modified 2'
+    mosky_detail['val', 0] = 'this email is modified'
+    mosky_detail.save()
+
+    mosky_detail = Detail.select(where={'person_id': 'mosky', 'key': 'email'})
+    print mosky_detail['val']
+    print mosky_detail['val', 0]
+    print
