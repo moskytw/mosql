b78462508fa211ff302656e12c49f28f93a50a60
Mosky <mosky.tw@gmail.com>
2013-05-24 11:33:19 +0800

    use pool in sqlalchemy

--- examples/mysql/base.py
+++ examples/mysql/base.py
@@ -6,14 +6,18 @@ import MySQLdb
 from mosql.result import Model
 import mosql.mysql # a patch for MySQL's non-standard syntax
 
+try:
+    import sqlalchemy.pool
+    MySQLdb = sqlalchemy.pool.manage(MySQLdb, pool_size=5)
+except ImportError:
+    pass
+
 class MySQL(Model):
 
     @classmethod
     def getconn(cls):
-        if not hasattr(cls, '_conn'):
-            cls._conn = MySQLdb.connect(user='root', db='mosky')
-        return cls._conn
+        return MySQLdb.connect(user='root', db='mosky')
 
     @classmethod
     def putconn(cls, conn):
-        pass
+        conn.close()
