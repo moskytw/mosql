2c13961d9effb54efd259a2909af3b704a73a09e
Mosky <mosky.tw@gmail.com>
2013-05-28 03:48:31 +0800

    added instructions of security

--- examples/mysql/base.py
+++ examples/mysql/base.py
@@ -21,7 +21,11 @@ class MySQL(Model):
 
     @classmethod
     def getconn(cls):
-        return MySQLdb.connect(user='root', db='mosky')
+        # The ``charset='utf8'`` is just ensure we connect db with safe encoding
+        # If ``show variables where variable_name = 'character_set_connection';`` shows ``utf-8``,
+        # you can ignore this.
+        # The ``use_unicode=False`` is just for the consistency with another example.
+        return MySQLdb.connect(user='root', db='mosky', charset='utf8', use_unicode=False)
 
     @classmethod
     def putconn(cls, conn):
--- examples/postgresql/base.py
+++ examples/postgresql/base.py
@@ -7,7 +7,10 @@ from mosql.result import Model
 # if you want to use the native escape function
 #import mosql.psycopg2_escape
 
-pool = psycopg2.pool.SimpleConnectionPool(1, 5, database='mosky')
+# The `client_encoding`='utf-8'`` is just ensure we connect db with safe encoding.
+# If ``show client_encoding;` shows ``utf-8``,
+# you can ignore this.
+pool = psycopg2.pool.SimpleConnectionPool(1, 5, database='mosky', client_encoding='utf-8')
 
 class PostgreSQL(Model):
 
