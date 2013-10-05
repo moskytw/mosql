e1e5e6e1b386ecfede2b87eaa744b8994f2b9a54
Mosky <mosky.tw@gmail.com>
2013-10-05 10:02:23 +0800

    removed the blank lines

--- examples/60_class.py
+++ examples/60_class.py
@@ -8,15 +8,12 @@ from mosql.db import Database, one_to_dict
 class Person(dict):
 
     db = Database(psycopg2, host='127.0.0.1')
-
     table_info = {'table': 'person'}
     select = select.breed(table_info)
     insert = insert.breed(table_info)
 
     def __init__(self, *args, **kargs):
-
         dict.__init__(self, *args, **kargs)
-
         row_info = self.table_info.copy()
         row_info['where'] = {'person_id': self['person_id']}
         self.update = update.breed(row_info)
@@ -24,26 +21,19 @@ class Person(dict):
 
     @classmethod
     def create(cls, *args, **kargs):
-
         person = cls(*args, **kargs)
-
         with cls.db as cur:
             cur.execute(cls.insert(set=person))
-
         return person
 
     @classmethod
     def fetch(cls, person_id):
-
         with cls.db as cur:
-
             cur.execute(cls.select(where={'person_id': person_id}))
-
             if cur.rowcount:
                 person = cls(one_to_dict(cur))
             else:
                 person = None
-
         return person
 
     def save(self):
